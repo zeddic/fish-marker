0ca05ad2d107296a75e20f392715c4160812d1d6
LewBlu <lewis.blundell.development@gmail.com>
2023-01-10 19:05:37 +0000

    Icons added to mobile nav

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -20,7 +21,8 @@ import { MobileComponent } from './navigation/mobile/mobile.component';
 	],
 	imports: [
 		BrowserModule,
-		AppRoutingModule
+		AppRoutingModule,
+		FontAwesomeModule
 	],
 	providers: [],
 	bootstrap: [AppComponent]
--- src/app/navigation/mobile/mobile.component.ts
+++ src/app/navigation/mobile/mobile.component.ts
@@ -1,10 +1,13 @@
 import { Component } from '@angular/core';
+import { faMapLocationDot, faNewspaper, faUser } from '@fortawesome/free-solid-svg-icons';
 
 @Component({
-  selector: 'app-mobile',
-  templateUrl: './mobile.component.html',
-  styleUrls: ['./mobile.component.css']
+	selector: 'app-mobile',
+	templateUrl: './mobile.component.html',
+	styleUrls: ['./mobile.component.css']
 })
 export class MobileComponent {
-
+	faMap = faMapLocationDot;
+	faNewspaper = faNewspaper;
+	faUser = faUser;
 }
