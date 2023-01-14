7addfb63dee9cab64c949688b33eff87cde9a075
LewBlu <lewis.blundell.development@gmail.com>
2023-01-14 22:30:16 +0000

    Added route guards for authenticated users

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,14 +1,17 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AngularFireAuthGuard, redirectUnauthorizedTo } from '@angular/fire/compat/auth-guard';
 import { LoginComponent } from './auth/login/login.component';
 import { CatchesComponent } from './catches/catches.component';
 import { MapComponent } from './map/map.component';
 import { ProfileComponent } from './profile/profile.component';
 
+const redirectUnauthorizedToLogin = () => redirectUnauthorizedTo(['login']);
+
 const routes: Routes = [
-	{ path: 'map', component: MapComponent },
-	{ path: 'catches', component: CatchesComponent },
-	{ path: 'profile', component: ProfileComponent },
+	{ path: 'map', component: MapComponent, canActivate: [AngularFireAuthGuard], data: { authGuardPipe: redirectUnauthorizedToLogin } },
+	{ path: 'catches', component: CatchesComponent, canActivate: [AngularFireAuthGuard], data: { authGuardPipe: redirectUnauthorizedToLogin } },
+	{ path: 'profile', component: ProfileComponent, canActivate: [AngularFireAuthGuard], data: { authGuardPipe: redirectUnauthorizedToLogin } },
 	{ path: 'login', component: LoginComponent },
 	{ path: '**', redirectTo: '/catches' }
 ];
