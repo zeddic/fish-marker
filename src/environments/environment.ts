970e1f26cc14d6bba07c3c36ba483c7a75a6fa9b
LewBlu <lewis.blundell.development@gmail.com>
2023-01-19 19:07:54 +0000

    Update to action

---
+++ src/environments/environment.ts
@@ -0,0 +1,11 @@
+export const environment = {
+	firebase: {
+		projectId: 'fish-marker-7e84b',
+		appId: '1:162776604040:web:e36f331310075e998d6e3d',
+		storageBucket: 'fish-marker-7e84b.appspot.com',
+		apiKey: 'AIzaSyCVfZKF9MZ4c6u2GCw5G69k3BoDLemZVDA',
+		authDomain: 'fish-marker-7e84b.firebaseapp.com',
+		messagingSenderId: '162776604040',
+		measurementId: 'G-1KDV9Q6TFX',
+	},
+};
