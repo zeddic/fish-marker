e6bcf3cec4d24e6e7b5044716888b9d5fe6d4634
LewBlu <lewis.blundell.development@gmail.com>
2023-01-22 13:06:41 +0000

    Created user service to manage user

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
-import { User } from '@angular/fire/auth/firebase';
 import { AngularFireAuth } from '@angular/fire/compat/auth';
+import { UserService } from './auth/user.service';
 
 @Component({
 	selector: 'app-root',
@@ -9,18 +9,17 @@ import { AngularFireAuth } from '@angular/fire/compat/auth';
 })
 export class AppComponent implements OnInit {
 	title = 'fish-marker';
-	constructor(private auth: AngularFireAuth) { }
+	constructor(private auth: AngularFireAuth, private userService: UserService) { }
 	showNavigation = false;
-	user = {};
 
 	ngOnInit(): void {
 		this.auth.onAuthStateChanged(user => {
 			if (user) {
 				this.showNavigation = true;
-				this.user = user;
+				this.userService.userDetails.next(user);
 			} else {
 				this.showNavigation = false;
-				this.user = {};
+				this.userService.userDetails.next({});
 			}
 		});
 	}
--- src/app/map/map.component.ts
+++ src/app/map/map.component.ts
@@ -4,8 +4,8 @@ import { Modal } from 'bootstrap';
 import { AngularFirestore, AngularFirestoreCollection } from '@angular/fire/compat/firestore';
 import { first } from 'rxjs';
 import { FormControl, FormGroup, Validators } from '@angular/forms';
-import { AngularFireAuth } from '@angular/fire/compat/auth';
 import { environment } from 'src/environments/environment';
+import { UserService } from '../auth/user.service';
 
 @Component({
 	selector: 'app-map',
@@ -19,7 +19,7 @@ export class MapComponent implements OnInit {
 	catchesCollection!: AngularFirestoreCollection;
 	catches: any[] = [];
 	map!: any;
-	uid!: any;
+	user!: any;
 
 	catchForm = new FormGroup({
 		'type': new FormControl('', { nonNullable: true, validators: [Validators.required] }),
@@ -30,7 +30,7 @@ export class MapComponent implements OnInit {
 		'lng': new FormControl('', { nonNullable: true, validators: [Validators.required] })
 	});
 
-	constructor(private firestore: AngularFirestore, private auth: AngularFireAuth) { }
+	constructor(private firestore: AngularFirestore, private userService: UserService) { }
 
 	ngOnInit() {
 		// Initialise loaded for map
@@ -38,21 +38,33 @@ export class MapComponent implements OnInit {
 			apiKey: environment.firebase.mapApiKey
 		});
 
-		this.auth.authState.pipe(first()).subscribe(user => {
-			this.uid = user?.uid;
+		// Fetch the user details
+		this.userService.userDetails.pipe(first()).subscribe(user => {
+			this.user = user;
 
+			// Load the map
 			loader.load().then(() => {
-				// Initialise Map
+				// Initialise Map and click event on map
 				this.map = new google.maps.Map(document.getElementById('map')!, {
 					center: { lat: 53.391991, lng: -3.178860 },
 					zoom: 20,
 					mapTypeId: 'satellite',
 					tilt: 0
+				}).addListener('click', (mapsMouseEvent: any) => {
+					let location = mapsMouseEvent.latLng.toJSON();
+					// Add click location to marker form
+					this.catchForm.patchValue({
+						lat: location.lat,
+						lng: location.lng
+					});
+
+					const element = document.getElementById('add-catch-modal') as HTMLElement;
+					const myModal = new Modal(element);
+					myModal.show();
 				});
 
 				// Fetch list of catches from firebase
-
-				this.catchesCollection = this.firestore.collection('catches', ref => ref.where('uid', '==', this.uid));
+				this.catchesCollection = this.firestore.collection('catches', ref => ref.where('uid', '==', this.user.uid));
 				this.catchesCollection.valueChanges({ idField: 'doc_id' }).pipe(first()).subscribe(documents => {
 					// Save documents to catches array
 					this.catches = documents;
@@ -84,20 +96,6 @@ export class MapComponent implements OnInit {
 						});
 					}
 				});
-
-				// On click of map, add new marker to map
-				this.map.addListener('click', (mapsMouseEvent: any) => {
-					let location = mapsMouseEvent.latLng.toJSON();
-					// Add click location to marker form
-					this.catchForm.patchValue({
-						lat: location.lat,
-						lng: location.lng
-					});
-
-					const element = document.getElementById('add-catch-modal') as HTMLElement;
-					const myModal = new Modal(element);
-					myModal.show();
-				});
 			});
 		});
 	}
@@ -106,7 +104,7 @@ export class MapComponent implements OnInit {
 		if (this.catchForm.valid) {
 			// Get values of form then and user id
 			let formValues: any = this.catchForm.value;
-			formValues.uid = this.uid;
+			formValues.uid = this.user.uid;
 			// Send data to firebase
 			this.catchesCollection.add(formValues).then(res => {
 				// Once added clear form and add marker to map with click event
