0475c59f60432d440ef113fd9f9f39b6a063b6bc
LewBlu <lewis.blundell.development@gmail.com>
2023-01-29 12:00:39 +0000

    Profile component refactor

--- src/app/profile/profile.component.ts
+++ src/app/profile/profile.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { User } from '@angular/fire/auth';
 import { AngularFireAuth } from '@angular/fire/compat/auth';
 import { Router } from '@angular/router';
 import { faCog, faRightFromBracket } from '@fortawesome/free-solid-svg-icons';
@@ -13,11 +14,10 @@ import { CatchesService } from '../catches/catches.service';
 	styleUrls: ['./profile.component.css']
 })
 
-export class ProfileComponent implements OnInit {
-	faCog = faCog;
-	faLogOut = faRightFromBracket;
+export class ProfileComponent implements OnInit, OnDestroy {
+	icons = { 'faCog': faCog, 'faLogOut': faRightFromBracket };
 	userSubscription!: Subscription;
-	user: any;
+	user!: User;
 	catches!: Catch[];
 	personalBest = { lbs: 0, oz: 0 };
 	count = 0;
@@ -25,13 +25,15 @@ export class ProfileComponent implements OnInit {
 	constructor(private auth: AngularFireAuth, private router: Router, private userService: UserService, private catchesService: CatchesService) { }
 
 	ngOnInit(): void {
-		this.userSubscription = this.userService.user.subscribe(user => {
-			this.user = user;
+		this.userSubscription = this.userService.user.subscribe((user: User | null) => {
+			if (user !== null) {
+				this.user = user;
+			}
 		});
 
-		this.userSubscription = this.catchesService.userCatches.subscribe(catches => {
+		this.userSubscription = this.catchesService.userCatches.subscribe((catches: Catch[] | null) => {
 			if (catches !== null) {
-				catches.forEach((item: any) => {
+				catches.forEach((item: Catch) => {
 					if (item.lbs >= this.personalBest.lbs) {
 						this.personalBest = { lbs: item.lbs, oz: (item.oz > this.personalBest.oz ? item.oz : this.personalBest.oz) };
 					}
@@ -46,5 +48,9 @@ export class ProfileComponent implements OnInit {
 		this.userSubscription.unsubscribe();
 		this.auth.signOut();
 		this.router.navigate(['login']);
+	}
+
+	ngOnDestroy(): void {
+		this.userSubscription.unsubscribe();
 	}
 }
